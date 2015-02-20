a98fc7927b87dc6a1749d9e06236ed99f728e417
Tony Di Croce <dicroce@gmail.com>
2015-02-20 08:22:44 -0800

    updated file headers.

--- include/GIT.h
+++ include/GIT.h
@@ -1,4 +1,14 @@
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// XSDK
+// Copyright (c) 2015 Schneider Electric
+//
+// Use, modification, and distribution is subject to the Boost Software License,
+// Version 1.0 (See accompanying file LICENSE).
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #ifndef __Buildy__GIT_h
 #define __Buildy__GIT_h
 
